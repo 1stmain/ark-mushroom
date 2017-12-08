c8c360de005423dbe40fa43551f08b9b44c5a397
abhi6823 <abhilash@1stmain.co>
2017-12-08 01:36:17 -0800

    click gesture

--- src/KinectHelper.java
+++ src/KinectHelper.java
@@ -4,7 +4,11 @@ import edu.ufl.digitalworlds.j4k.Skeleton;
 public class KinectHelper extends J4KSDK
 {
     private KinectHelperCallback kinectHelperCallback;
-
+    public float oldRightX =0 ;
+    public float oldRightY = 0;
+    public float oldRightZ = 0;
+    public int handpush = 0;
+    public int intialise = 0;
     KinectHelper(KinectHelperCallback kinectHelperCallback)
     {
         if (!System.getProperty("os.arch").toLowerCase().contains("64"))
@@ -58,9 +62,29 @@ public class KinectHelper extends J4KSDK
 
         float rightHandX = skeleton.get2DJoint(Skeleton.HAND_RIGHT, Constants.STAGE_WIDTH, Constants.STAGE_HEIGHT)[0];
         float rightHandY = skeleton.get2DJoint(Skeleton.HAND_RIGHT, Constants.STAGE_WIDTH, Constants.STAGE_HEIGHT)[1];
-
-        System.out.println(rightHandX + ", " + rightHandY);
-
-        kinectHelperCallback.onRightHandMoved(rightHandX, rightHandY);
-    }
+        float rightHandZ = skeleton.get3DJointZ(Skeleton.HAND_RIGHT);
+//        System.out.println(rightHandZ);
+        if(intialise == 0){
+                oldRightZ = rightHandZ;
+            intialise = 1;
+        }
+//        System.out.println(oldRightZ + "" + rightHandZ);
+//                if(oldRightY < rightHandY && rightHandY - oldRightY > 200 ){
+//                    System.out.println("swipe down ");
+//                }
+//                if(oldRightX < rightHandX && rightHandX - oldRightX > 200){
+//                    System.out.println("swripe right");
+//                }
+//                System.out.println(handpush + "" + rightHandZ + " " + oldRightZ );
+               if(rightHandZ < oldRightZ && oldRightZ -  rightHandZ  > 0.4){
+                   System.out.println("hand on ");
+                    handpush  =1;
+                }
+               if( oldRightZ - rightHandZ  < 0.01 && handpush == 1 ){
+                    System.out.println("hand off ");
+                    handpush = 0 ;
+              }
+                oldRightX = rightHandX;
+                oldRightY  = rightHandY;
+        }
 }
